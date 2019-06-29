8825fa5d7e38cd5da527f4096af1c73edb48be79
jiangzhengshen <jiangzhengshen@163.com>
2019-06-29 21:10:07 +0800

    icml

--- Crawler/crawler_enhanced.py
+++ Crawler/crawler_enhanced.py
@@ -1,15 +1,14 @@
-import json
-import urllib
 import argparse
-import logging
-from pyquery import PyQuery
-import socket
-import time
-import queue
-import sys
-import traceback
 import hashlib
+import logging
 import os
+import queue
+import socket
+from urllib.parse import quote, urlsplit
+from urllib.request import Request, urlopen
+from urllib.error import URLError
+
+from pyquery import PyQuery
 
 ''' 根据输入的网址和深度爬网页
     需要对php网页取得真实的网站地址
@@ -19,14 +18,15 @@ import os
         URLCrawler: 结果存储为收藏夹的HTML格式
 '''
 
+
 class Crawler(object):
     def __init__(self, args):
-        ''' log设置 '''
+        # log设置
         log_file = 'crawler.log'
-        logging.basicConfig(filename = log_file,
-                    format = '%(asctime)s -> %(levelname)s %(message)s',
-                    datefmt = '%Y/%m/%d %H:%M:%S',
-                    level = logging.DEBUG)
+        logging.basicConfig(filename=log_file,
+                            format='%(asctime)s -> %(levelname)s %(message)s',
+                            datefmt='%Y/%m/%d %H:%M:%S',
+                            level=logging.DEBUG)
         logging.info('\n=================== New Session =====================')
 
         ''' 对整个socket层设置超时时间(s)。后续文件中如果再使用到socket，不必再设置 '''
@@ -41,20 +41,22 @@ class Crawler(object):
             os.mkdir(self._out_dir)
 
         self.current_depth = 0
-        self.url_queue = queue.Queue() # 待爬取的url队列，格式(url, depth)
+        self.url_queue = queue.Queue()  # 待爬取的url队列，格式(url, depth)
         for url in self._init_urls:
             self.url_queue.put((url, self.current_depth))
-        
-        self.cached_urls = {} # 所有爬过网页的原始url，格式url -> [cnt, depth]
 
-    def __get_html(self, url):
+        self.cached_urls = {}  # 所有爬过网页的原始url，格式url -> [cnt, depth]
+
+    @staticmethod
+    def __get_html(url):
         try:
-            return PyQuery(url = url, parser = 'html')
+            return PyQuery(url=url, parser='html')
         except Exception as e:
             logging.warning('PyQuery: %s : %s\n\tURL: %s', type(e), e, url)
             return None
 
-    def __get_real_url(self, raw_url):
+    @staticmethod
+    def __get_real_url(raw_url):
         try:
             '''
             参考了js中encodeURI的不编码字符
@@ -62,13 +64,13 @@ class Crawler(object):
             encodeURI不编码字符有82个：!，#，$，&，'，(，)，*，+，,，-，.，/，:，;，=，?，@，_，~，0-9，a-z，A-Z
             encodeURIComponent不编码字符有71个：!， '，(，)，*，-，.，_，~，0-9，a-z，A-Z
             '''
-            url = urllib.parse.quote(raw_url, safe = '!#$&()*+,-./:;=?@_~\'', encoding = 'utf-8')
-            req = urllib.request.Request(url)
-            response = urllib.request.urlopen(req)
+            url = quote(raw_url, safe='!#$&()*+,-./:;=?@_~\'', encoding='utf-8')
+            req = Request(url)
+            response = urlopen(req)
             new_url = response.geturl()
             response.close()
             return new_url
-        except urllib.error.URLError as e:
+        except URLError as e:
             logging.warning('Request: URLError: %s\n\tRaw URL: %s', e.reason, raw_url)
             return ''
         except Exception as e:
@@ -79,7 +81,7 @@ class Crawler(object):
         """ extract all the urls from html, except for the cached urls """
         try:
             html_pyquery.make_links_absolute()
-            all_urls = html_pyquery('a').map(lambda i, e: PyQuery(e)('a').attr('href'))
+            all_urls = html_pyquery('a').map(lambda i, element: PyQuery(element)('a').attr('href'))
             url_list = set()
             for url in all_urls:
                 real_url = self.__get_real_url(url)
@@ -97,9 +99,11 @@ class Crawler(object):
     def __dump_cached_urls(self):
         with open('cached_urls.txt', 'w') as dump_file:
             for url in self.cached_urls:
-                dump_file.write(url + '\t' + str(self.cached_urls[url][0]) + '\t' + str(self.cached_urls[url][1]) + '\n')
+                dump_file.write(
+                    url + '\t' + str(self.cached_urls[url][0]) + '\t' + str(self.cached_urls[url][1]) + '\n')
 
-    def filter_url(self, urls):
+    @staticmethod
+    def filter_url(urls):
         """ could be personalized implemented """
         return urls
 
@@ -131,7 +135,9 @@ class Crawler(object):
                 self.cached_urls[url] = [1, depth]
 
             ''' extract urls from the html content, except for the cached urls '''
-            extracted_urls = self.__extract_url(html_pyquery)
+            extracted_urls = []
+            if self.current_depth < self._depth:
+                extracted_urls = self.__extract_url(html_pyquery)
 
             ''' only retain the needed urls, and put them into the queue '''
             filtered_urls = self.filter_url(extracted_urls)
@@ -140,27 +146,29 @@ class Crawler(object):
 
         self.__dump_cached_urls()
 
+
 class URLCrawler(Crawler):
     def save_content(self, url, depth, html_pyquery):
-        parseList = urllib.parse.urlsplit(url)
-        host = parseList[0] + '://' + parseList[1]
+        parse_list = urlsplit(url)
+        host = parse_list[0] + '://' + parse_list[1]
         with open(self._out_dir + 'savedLinks.txt', 'a') as outfile:
             outfile.write(host + '\n')
 
+
 class ImageCrawler(Crawler):
     def save_content(self, url, depth, html_pyquery):
-        all_imgs = html_pyquery('img').map(lambda i, e: PyQuery(e)('img').attr('src'))
-        for img_url in all_imgs:
-            image_name = img_url.split('/')[-1]
+        all_imgs = html_pyquery('img').map(lambda i, element: PyQuery(element)('img').attr('src'))
+        for raw_url in all_imgs:
+            image_name = raw_url.split('/')[-1]
             words = image_name.split('.')
             suffix = ''
             if len(words) > 1:
                 suffix = words[-1]
             print(image_name + ', ' + suffix)
             try:
-                img_url = urllib.parse.quote(img_url, safe = '!#$&()*+,-./:;=?@_~\'', encoding = 'utf-8')
-                req = urllib.request.Request(img_url)
-                response = urllib.request.urlopen(req)
+                img_url = quote(raw_url, safe='!#$&()*+,-./:;=?@_~\'', encoding='utf-8')
+                req = Request(img_url)
+                response = urlopen(req)
                 content = response.read()
 
                 m = hashlib.md5()
@@ -171,28 +179,48 @@ class ImageCrawler(Crawler):
                     continue
                 with open(self._out_dir + filename, 'wb') as image_file:
                     image_file.write(content)
-            except urllib.error.URLError as e:
+            except URLError as e:
                 logging.warning('Request: URLError: %s\n\tRaw URL: %s', e.reason, raw_url)
                 continue
             except Exception as e:
                 logging.warning('Request: %s : %s\n\tRaw URL: %s', type(e), e, raw_url)
                 continue
 
-def main():
-    if sys.modules['idlelib']:
-        sys.argv.extend(input("Args: ").split())
-        
-    '''args.init_urls = 'http://www.baidu.com'
-    args.depth = 3'''
 
-    parser = argparse.ArgumentParser(description = 'A crawler for website') 
-    parser.add_argument('-a', type=str, required=True, metavar='WebAddr', dest='init_urls', help='Specify the Website Address')
+class ICML2019Crawler(Crawler):
+    @staticmethod
+    def heading_author(_, element):
+        entry = PyQuery(element)
+        heading = entry('.maincardBody').text()
+        author_list = entry('.maincardFooter').text().split(' · ')
+        return heading, author_list
+
+    def save_content(self, url, depth, html_pyquery):
+        all_entries = html_pyquery('.maincard')
+        heading_authors = all_entries.map(ICML2019Crawler.heading_author)
+        with open(self._out_dir + 'ICML2019.csv', 'w', encoding='utf8') as outfile:
+            for heading, author_list in heading_authors:
+                outfile.write('\t'.join([heading] + author_list) + '\n')
+
+
+def main():
+    # if sys.modules['idlelib']:
+    #     sys.argv.extend(input("Args: ").split())
+
+    # args.init_urls = 'http://www.baidu.com'
+    # args.depth = 3
+
+    parser = argparse.ArgumentParser(description='A crawler for website')
+    parser.add_argument('-a', type=str, required=True, metavar='WebAddr', dest='init_urls',
+                        help='Specify the Website Address')
     parser.add_argument('-d', type=int, default=1, metavar='CrawlDepth', dest='depth', help='Specify the Crawler Depth')
-    parser.add_argument('-o', type=str, default='./', metavar='OutputDir', dest='out_dir', help='Specify the Output Directory')
+    parser.add_argument('-o', type=str, default='./', metavar='OutputDir', dest='out_dir',
+                        help='Specify the Output Directory')
     args = parser.parse_args()
 
-    crawler = URLCrawler(args)
+    crawler = ICML2019Crawler(args)
     crawler.run()
+
 
 if __name__ == '__main__':
     main()
